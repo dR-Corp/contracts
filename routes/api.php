5258a4cfe4ded857881690e5c05e45611758f76d
RAYID DJERI <rayidjeri@gmail.com>
2024-04-04 18:14:12 +0100

    Adding RESTful API routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ContratController;
+use App\Http\Controllers\PaiementController;
+use App\Http\Controllers\PrestataireController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\TacheController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +23,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('roles', RoleController::class)->except(['create', 'edit']);
+Route::resource('users', AuthController::class)->except(['create', 'edit']);
+Route::resource('prestataires', PrestataireController::class)->except(['create', 'edit']);
+Route::resource('contrats', ContratController::class)->except(['create', 'edit']);
+Route::resource('taches', TacheController::class)->except(['create', 'edit']);
+Route::resource('paiements', PaiementController::class)->except(['create', 'edit']);
